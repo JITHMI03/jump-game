1ccce3132063630af6e87c7d9669b2bdd9523d2c
Welcome <malshajithmi715@gmail.com>
2026-02-25 23:13:17 +0530

    init

---
+++ game_manager.gd
@@ -0,0 +1,23 @@
+extends Node
+
+@onready var pointslabel: Label = %pointslabel
+@export var hearts : Array[Node]
+
+var points = 0
+var lives = 3
+
+func decrease_health():
+	lives -= 1
+	print(lives)
+	for h in 3:
+		if (h < lives):
+			hearts[h].show()
+		else:
+			hearts[h].hide()
+	if (lives == 0):
+		get_tree().reload_current_scene()
+
+func add_point():
+	points += 1
+	print(points)
+	pointslabel.text = "Collected: " + str(points) 
